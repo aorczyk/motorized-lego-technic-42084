adc38505523c05be23e976615bc38b3bfdb8d98a
Adam Orczyk <adam.orczyk@gmail.com>
2024-11-10 12:39:54 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 let latestCommands: { [key: string]: number } = {}
-let led1 = false;
-let led2 = false;
+let mode = 1;
 
 basic.clearScreen()
 
@@ -23,37 +22,30 @@ basic.forever(function () {
         delete latestCommands[commandName];
 
         if (commandName == "-v") {
+            bluetooth.uartWriteLine('vc;import_start;')
             bluetooth.uartWriteLine('vc;init;')
             bluetooth.uartWriteLine('vc;sl;1;-100;100;1;1;0;1;;')
-            bluetooth.uartWriteLine('vc;sr;1;150;210;1;0;0;0;;')
-            bluetooth.uartWriteLine('vc;ox;0;-45;45;-7;7;1;0;0;')
-            bluetooth.uartWriteLine('vc;oy;0;-45;45;-7;7;1;0;0;')
+            bluetooth.uartWriteLine('vc;jry;-100;100;1;0;0;')
+            bluetooth.uartWriteLine('vc;b;Digit1;1;0;<i class="fa-solid fa-volume-high"></i>;')
+            bluetooth.uartWriteLine('vc;b;Digit2;0;0;2;')
+            bluetooth.uartWriteLine('vc;b;Digit4;0;0;4;')
+            bluetooth.uartWriteLine('vc;oy;0;-30;30;-100;100;10;0;0;')
             bluetooth.uartWriteLine('vc;il;1;')
             bluetooth.uartWriteLine('vc;ir;1;')
-            bluetooth.uartWriteLine('vc;show;sl,sr,br;')
+            bluetooth.uartWriteLine('vc;sr;1;-30;30;5;1;0;0;;')
+            bluetooth.uartWriteLine('vc;ox;1;-30;30;-30;30;5;1;0;')
+            bluetooth.uartWriteLine('vc;jrx;-30;30;5;1;0;')
+            bluetooth.uartWriteLine('vc;show;sl,sr,jr,br;')
+            bluetooth.uartWriteLine('vc;import_end;')
         } else if (commandName == "oy" || commandName == "sl" || commandName == "jry") {
             wuKong.setMotorSpeed(wuKong.MotorList.M1, commandValue)
         } else if (commandName == "ox" || commandName == "sr" || commandName == "jrx") {
-            wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, commandValue)
+            let value = commandValue + 180;
+            if (value >= 150 && value <= 210) {
+                wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, value)
+            }
         } else if (commandName == "1") {
-            if (led1) {
-                wuKong.setLightMode(wuKong.LightMode.OFF)
-            } else {
-                wuKong.setLightMode(wuKong.LightMode.BREATH)
-            }
-
-            led1 = !led1
-        } else if (commandName == "2") {
-            let myStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-
-            if (led2) {
-                myStrip.clear()
-                myStrip.show()
-            } else {
-                myStrip.showColor(neopixel.colors(NeoPixelColors.Yellow))
-            }
-
-            led2 = !led2
+            music.playTone(250, 500)
         }
     }
 })
